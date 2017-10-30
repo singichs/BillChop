a830d0b35f5c078af8cb01e7ad2f28d68ee4f117
Peter Kaplan <pkap@umich.edu>
2017-10-30 15:21:49 -0400

    maybe working models?

--- BillChop/chop/models.py
+++ BillChop/chop/models.py
@@ -7,50 +7,51 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
-
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
     venmo_handle = models.CharField(max_length=30)
     phone_number = PhoneNumberField()
-    groups = models.ManyToManyField(Group)
-    outstanding_payments = models.ManyToManyField(Payment)
-    receipts = models.ManyToManyField(Receipt)
+    groups = models.ManyToManyField(Group, through='UserMembership', related_name='people')
+    receipts = models.ManyToManyField(Receipt, through='ReceiptMembership', related_name='receiptmany')
 
 class Receipt(models.Model):
-
-    photo_id = 
+    photo_bucket = models.CharField(max_length=30)
     timestamp = models.DateField(auto_now_add = True)
     total_cost = models.DecimalField()
     tip = models.DecimalField()
     tax = models.DecimalField()
-    items = 
+    is_complete = models.BooleanField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
 
 class Item(models.Model):
-
     name = models.CharField(max_length=30)
     value = models.DecimalField()
-    receipt_id = models.ForeignKey(Receipt, on_delete=models.CASCADE)
-    belongs_to = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
+    user_owns = models.ForeignKey(User, on_delete=models.CASCADE)
     # make functions that return cost per user
 
 class Group(models.Model):
-    
     name = models.CharField(max_length=30)
-    users = 
-    payments = 
+    date_created = models.DateField()
+    last_used = models.DateField()
+
+class UserMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    role = models.CharField(max_length=30)
+
+class ReceiptMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
+    outstanding_payment = models.DecimalField()
 
 
-class Payment(models.Model):
-    receipt_id = 
-    paid_by = 
-    is_complete = models.BooleanField()
-    split = 
 
-class Split(models.Model):
-    user_id = 
-    cost = models.DecimalField()
 
-    #Probably need some user group tables
+
+
+
