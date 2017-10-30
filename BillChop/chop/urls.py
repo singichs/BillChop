bc44a637e04ee7524bdeab7c8825639ab6000563
jmkunnat <jmkunnat@umich.edu>
2017-10-30 00:18:12 -0400

    extremely basic api outline

--- BillChop/chop/urls.py
+++ BillChop/chop/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
+# can move the url functions to different places
+# can include whatever in the actual url, receipt shown as example
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'^receipt/(?P<user_id>\d+)/$', views.receipt, name='receipt'),
+	url(r'^get_receipt', views.get_receipt, name='get_receipt'),
+	url(r'^create_group', views.create_group, name='create_group'),
+	url(r'^group', views.group, name='group'),
+	url(r'^payments', views.payments, name='payments'),
+	url(r'^payup', views.payup, name='payup'),
 ]
--- BillChop/chop/views.py
+++ BillChop/chop/views.py
@@ -4,7 +4,52 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+
+# all functions with post have "@csrf_exempt" for right now, couldn't test otherwise
 
 def index(request):
     return HttpResponse("Hello, world")
+
+# localhost:8000/chop/receipt/15/
+# note trailing slash, we need to include for POST requests while APPEND_SLASH is true		
+@csrf_exempt
+def receipt(request, user_id):
+	if request.method == "GET":
+		return HttpResponse("Receipt GET: user_id = " + user_id)
+	elif request.method == "POST":
+		return HttpResponse(request);
+
+@csrf_exempt
+def get_receipt(request):
+	if request.method == "POST":
+		return HttpResponse(request);
+
+@csrf_exempt
+def create_group(request):
+	if request.method == "POST":
+		return HttpResponse(request);
+
+@csrf_exempt
+def group(request):
+	if request.method == "GET":
+		return HttpResponse("Group GET");
+	elif request.method == "PUT":
+		return HttpResponse(request);
+	elif request.method == "POST":
+		return HttpResponse(request);
+
+@csrf_exempt
+def payments(request):
+	if request.method == "GET":
+		return HttpResponse("Payments GET");
+
+@csrf_exempt
+def payup(request):
+	if request.method == "POST":
+		return HttpResponse(request);
+
+
+
+
