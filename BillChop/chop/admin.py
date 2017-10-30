bb85a68873ddd747edeee1817b64067a79529623
Peter Kaplan <pkap@umich.edu>
2017-10-30 16:18:21 -0400

    Migrations

--- BillChop/chop/admin.py
+++ BillChop/chop/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from chop.models import Group, Receipt, User, Item, UserMembership, ReceiptMembership
+
 # Register your models here.
+
+
+admin.site.register(Group)
+admin.site.register(Receipt)
+admin.site.register(User)
+admin.site.register(Item)
+admin.site.register(UserMembership)
+admin.site.register(ReceiptMembership)
--- BillChop/chop/models.py
+++ BillChop/chop/models.py
@@ -7,48 +7,62 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    date_created = models.DateField(auto_now_add=True)
+    last_used = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name 
+
+class Receipt(models.Model):
+    photo_bucket = models.CharField(max_length=30)
+    timestamp = models.DateField(auto_now_add = True)
+    total_cost = models.DecimalField(max_digits=5, decimal_places=2)
+    tip = models.DecimalField(max_digits=5, decimal_places=2)
+    tax = models.DecimalField(max_digits=5, decimal_places=2)
+    is_complete = models.BooleanField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
     venmo_handle = models.CharField(max_length=30)
-    phone_number = PhoneNumberField()
+    #not working
+    #phone_number = PhoneNumberField()
     groups = models.ManyToManyField(Group, through='UserMembership', related_name='people')
     receipts = models.ManyToManyField(Receipt, through='ReceiptMembership', related_name='receiptmany')
 
-class Receipt(models.Model):
-    photo_bucket = models.CharField(max_length=30)
-    timestamp = models.DateField(auto_now_add = True)
-    total_cost = models.DecimalField()
-    tip = models.DecimalField()
-    tax = models.DecimalField()
-    is_complete = models.BooleanField()
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 class Item(models.Model):
     name = models.CharField(max_length=30)
-    value = models.DecimalField()
+    value = models.DecimalField(max_digits=5, decimal_places=2)
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
     user_owns = models.ForeignKey(User, on_delete=models.CASCADE)
     # make functions that return cost per user
-
-class Group(models.Model):
-    name = models.CharField(max_length=30)
-    date_created = models.DateField()
-    last_used = models.DateField()
+    def __str__(self):
+        return self.name 
 
 class UserMembership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateField()
     role = models.CharField(max_length=30)
+    def __str__(self):
+        return self.user.first_name + " " + self.group.name
+
 
 class ReceiptMembership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
-    outstanding_payment = models.DecimalField()
+    outstanding_payment = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return self.user.first_name + " " + str(self.receipt.timestamp)
 
 
 
