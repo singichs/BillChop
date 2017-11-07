e677607ec8f44bd30a81aff6aff4cd5231e3865d
kmatton <katiemat@umich.edu>
2017-11-07 18:29:08 -0500

    home screen with transaction items

--- frontEnd/BillChop/screens/Home.js
+++ frontEnd/BillChop/screens/Home.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import {
     Platform,
     StyleSheet,
+    FlatList,
     Text,
     View, Button, TouchableHighlight
 } from 'react-native';
+import { List, ListItem} from 'react-native-elements';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' +
@@ -12,6 +14,61 @@ const instructions = Platform.select({
     android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
+class TransactionList extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            loading: false,
+            data: [],
+            page: 1,
+            seed: 1,
+            error: null,
+            refreshing: false,
+        };
+    }
+
+    componentDidMount() {
+        this.makeRemoteRequest();
+    }
+
+    makeRemoteRequest = () => {
+        // for now, just dummy data later will actually make api request
+        // this link is useful: https://medium.com/react-native-development/
+        // how-to-use-the-flatlist-component-react-native-basics-92c482816fe6
+
+        const fake_data = [{"owner": "Ramana Keerthi", "cost": "40.00", "is_owner": true, "title": "Costco", "id": 0},
+            {"owner": "Mazen Oweiss", "cost": "123.00", "is_owner": false, "title": "Target", "id": 1},
+            {"owner": "Katie Matton", "cost": "84.34", "is_owner": true, "title": "Meijer", "id": 2}];
+        this.setState({data: fake_data});
+    };
+
+    render() {
+        getString = (item) => {
+            if (item.item.is_owner) {
+                return `You are owed $${item.item.cost}`;
+            }
+            return `${item.item.owner} owes you $${item.item.cost}`;
+        }
+        return (
+            <List>
+                <FlatList
+                    data={this.state.data}
+                    style={{flex: 1, flexDirection: 'row'}}
+                    renderItem={({ item }) => (
+                        <ListItem
+                            title={getString({item})}
+                            subtitle={item.title}
+                        />
+                    )}
+                    keyExtractor={item => item.id}
+                />
+            </List>
+        );
+    }
+}
+
 
 
 export default class Home extends Component<{}> {
@@ -21,22 +78,7 @@ export default class Home extends Component<{}> {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
-                    {instructions}
-                </Text>
-                <Text style={styles.instructions}>
-                    Hello, world!
-                </Text>
-                <Button
-                    onPress={() => this.props.navigation.navigate('Capture')}
-                    title="Capture Image"
-                />
+                <TransactionList/>
             </View>
         );
     }
